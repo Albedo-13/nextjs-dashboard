adfb6c38329be613456a7d5dcbdc4d3e8ada320b
PavelProkopenya <prokopenya.personal@gmail.com>
2023-12-18 11:04:16 +0300

    feat: seed database with initial data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 type TRootLayout = {
   readonly children: React.ReactNode;
@@ -8,6 +9,7 @@ type TRootLayout = {
 export default function RootLayout({ children }: TRootLayout) {
   return (
     <html lang="en">
+      <SpeedInsights />
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
